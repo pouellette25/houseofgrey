96fa920a9cb0b3ac383c44b4a9764a2736593917
pouellette25 <pouellette25@gmail.com>
2020-03-18 14:54:40 -0400

    Fixed the particle effects of the blacksmith forge

---
+++ Mods/Greys Prophecy/Scripts/BlockBlacksmithForge.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+public class BlockBlacksmithForge : BlockForge
+{
+    protected override void checkParticles(WorldBase _world, int _clrIdx, Vector3i _blockPos, BlockValue _blockValue)
+    {
+        var chunk = _world.GetChunkFromWorldPos(_blockPos) as Chunk;
+
+        if (chunk != null)
+        {
+            var blockEntity = chunk.GetBlockEntity(_blockPos);
+
+            if (blockEntity != null && blockEntity.bHasTransform)
+            {
+                var forgeLights = blockEntity.transform.FindInChilds("fl_forge_orange");
+
+                if(forgeLights == null)
+                {
+                    return;
+                }
+
+                forgeLights.gameObject.SetActive(_blockValue.meta != 0);
+            }
+        }
+    }
+}
